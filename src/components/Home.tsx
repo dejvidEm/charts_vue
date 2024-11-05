cc6b782496688e0f4dc3b73be11325cf5021b47d
dejvidEm <bithug10@gmail.com>
2024-11-05 18:07:34 +0100

    update homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,34 +1,35 @@
-import { useRef } from 'react';
+import { Link } from "react-router-dom";
 
-export default function Intro() {
-  const el = useRef<HTMLSpanElement>(null);
-
-
-
+const LandingPage = () => {
   return (
-    <section id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0">
-      <div className="flex items-center justify-center">
-        <div className="relative">
-          
-        </div>
-      </div>
-
-      <h1 className="mb-10 mt-4 px-6 text-lg font-medium !leading-[1.5] sm:text-2xl text-gray-950 dark:text-slate-100">
-        <p className="italic font-light text-sm sm:text-base my-4">„ Be the change that you wish to see in the world. "</p>
-        <span className="font-bold">Hello</span>, My name is <span className="font-bold">Dávid</span>, a tech geek based in Slovakia with <span className="font-bold">2 years</span> of experience. <br />
-        <span className="font-bold text-gray-600 dark:text-slate-300">I'm <span ref={el}></span></span>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-4xl font-bold mb-4 text-center text-blue-600">
+        Vitaj na stránke GraphApp
       </h1>
+      <p className="text-lg font-light text-center mb-8 max-w-md">
+        GraphApp je moderná webová aplikácia na jednoduché zobrazenie a analýzu
+        grafov. Pomocou našej aplikácie môžeš rýchlo a jednoducho vizualizovať
+        svoje dáta a získať z nich užitočné informácie.
+      </p>
 
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-4 px-0 text-md sm:text-lg font-medium">
-        <a href="#contact" className="group bg-gray-900 dark:bg-gray-700 text-white px-5 py-2 flex items-center gap-2 rounded-full hover:scale-105 transition-all">
-          Contact me here
-        </a>
-        <a href="/CV_2023_Mikulas.pdf" download className="group bg-white dark:bg-white/10 text-gray-900 dark:text-white/80 border border-gray-300 px-5 py-2 flex items-center gap-2 rounded-full hover:scale-105 transition-all">
-          Download CV 
-        </a>
+      {/* Sekcia Funkcií */}
+      <div className="text-center mb-10 space-y-6 max-w-lg">
+        <h2 className="text-2xl font-semibold text-gray-700">
+          Hlavné funkcie
+        </h2>
+        <ul className="list-none space-y-4 text-gray-600">
+          <li>- Intuitívne ovládanie a čistý dizajn</li>
+          <li>- Podpora rôznych typov grafov</li>
+          <li>- Reaguje na tvoje potreby v reálnom čase</li>
+          <li>- Optimalizované pre mobilné zariadenia</li>
+        </ul>
       </div>
 
-      
-    </section>
+      <div className="flex flex-col items-center space-y-4">
+        <Link to="/stranka1" className="text-white">Prehliadaj teraz</Link>
+      </div>
+    </div>
   );
-}
+};
+
+export default LandingPage;
